d4c578756855e458ec8374fe44e8af8703ea2bd7
Bia Plutarco <34174116+biaplutarco@users.noreply.github.com>
2025-04-03 16:56:34 -0300

    Initial Commit

---
+++ GhibliArchive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhibliArchive
+//
+//  Created by Beatriz Plutarco on 03/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
