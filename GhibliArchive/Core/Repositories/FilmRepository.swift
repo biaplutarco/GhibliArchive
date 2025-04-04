632d89fd9854c1b19269115b69375f492d06553b
Bia Plutarco <34174116+biaplutarco@users.noreply.github.com>
2025-04-04 13:04:20 -0300

    added repository

---
+++ GhibliArchive/Core/Repositories/FilmRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FilmRepository.swift
+//  GhibliArchive
+//
+//  Created by Beatriz Plutarco on 04/04/25.
+//
+
+import Foundation
---
+++ GhibliArchive/Core/Repositories/UserDefaultsFilmRepository.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsFilmRepository.swift
+//  GhibliArchive
+//
+//  Created by Beatriz Plutarco on 04/04/25.
+//
+
+import Foundation
